cf7de3d2eee58102e13cfe7dc14448cc64dc8325
Devika18 <devikankarpe@gmail.com>
2022-12-09 10:59:49 +0530

    added: logic for edit existing contact by name

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -1,20 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    static int contactID;
-    static Contact AddressBook[]=new Contact[10];
-
+    static ArrayList<Contact> AddressBook=new ArrayList();
+    static Scanner scanner=new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook System Program");
 
         AddressBookMain addressBook=new AddressBookMain();
-        Contact contact1=addressBook.createContact();
-        addressBook.addContact(contact1);
-        System.out.println(contact1);
+        Contact contact=addressBook.createContact();
+        addressBook.addContact(contact);
+        System.out.println(contact);
+
+        System.out.println("Enter Name to edit Contact");
+        String name=scanner.next();
+        addressBook.editContact(name);
+        System.out.println(contact);
     }
 
     Contact createContact(){
-        Scanner scanner=new Scanner(System.in);
         System.out.println("Enter first name");
         String firstName=scanner.next();
         System.out.println("Enter last name");
@@ -38,7 +42,31 @@ public class AddressBookMain {
     }
 
     void addContact(Contact contact){
-        AddressBook[contactID++]=contact;
+        AddressBook.add(contact);
         System.out.println("Contact added to AddressBook");
+    }
+    void editContact(String name){
+        for (Contact contact : AddressBook){
+            if (contact.firstName.equalsIgnoreCase(name)) {
+                System.out.println("Enter first name");
+                contact.firstName=scanner.next();
+                System.out.println("Enter last name");
+                contact.lastName=scanner.next();
+                System.out.println("Enter address");
+                contact.address=scanner.next();
+                System.out.println("Enter city");
+                contact.city=scanner.next();
+                System.out.println("Enter state");
+                contact.state=scanner.next();
+                System.out.println("Enter ZipCode");
+                contact.zipCode=scanner.nextInt();
+                System.out.println("Enter phoneNumber");
+                contact.phoneNumber=scanner.nextLong();
+                System.out.println("Enter Email");
+                contact.email=scanner.next();
+                System.out.println("Contact Updated successfully.");
+                break;
+            }
+        }
     }
 }
